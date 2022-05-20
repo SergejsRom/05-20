bb1e204ab3de29c64f22bbafc6f0db7d51af9f97
SergejsRom <sergejsromanovskis@gmail.com>
2022-05-20 11:01:43 +0300

    ...

---
+++ new.js
@@ -0,0 +1,13 @@
+/* 1. Tiesiogiai HTML faile rankiniu būdu sukurkite du tuščius <h2> tagus ir vieną mygtuką. (2 taškai)
+a. Paspaudus mygtuką kiekvienam tagui sugeneruotkite rand() atskirą skaičių nuo 1 iki 6 ir jį įrašykite į tago vidų naudojant innerText() metodą. (4 taškai)*/
+function getinnerText() {
+    let text = document.getElementById("myP").innerText;
+    document.getElementById("demo").innerText = text;
+  }
+
+
+/*b. Paspaudus mygtuką skaičiai turi būti pergeneruojami iš naujo. Jeigu sugeneruoti skaičiai yra vienodi, juos nudažyti raudonai. (4 taškai)
+*/
+
+
+console.log('labas133');
